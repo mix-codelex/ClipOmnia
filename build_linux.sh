b007596ccc3723cffd657baaff6cef6dd68af9e5
Benedith Mulongo <mulongobent@yahoo.fr>
2025-11-27 00:43:10 +0100

    Correct build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,4 @@
-docker build -t clipomnia-deb -f Dockerfile.build-deb .
+docker build -t clipomnia-deb -f Dockerfile.build-deb.auto .
 docker create --name clipomnia_tmp clipomnia-deb
 docker cp clipomnia_tmp:/output/clipomnia.deb .
 docker rm clipomnia_tmp
